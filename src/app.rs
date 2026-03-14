cec795f6f54a1b28f46e196fdffa58ca46150293
kai <chenkenny414@gmail.com>
2026-03-13 20:26:06 -0400

    editing -> serializing internal state

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-
+use std::collections::HashMap;
 
 //Three screens
 pub enum CurrentScreen{
@@ -19,3 +19,42 @@ pub struct App {
     pub current_screen: CurrentScreen,
     pub currently_editing: Option<CurrentlyEditing>,
 }
+impl App {
+    pub fn new() -> App {
+        App {
+            key_input: String::new(),
+            value_input: String::new(),
+            pairs: HashMap::new(),
+            current_screen: CurrentScreen::Main,
+            currently_editing: None,
+        }
+    }
+
+    pub fn save_key_value(&mut self){
+        self.pairs.insert(self.key_input.clone(), self.value_input.clone());
+
+        //Reset editing variables after appending
+        self.key_input = String::new();
+        self.value_input = String::new();
+        self.currently_editing = None;
+    }
+
+    pub fn toggle_editing(&mut self) {
+        if let Some(edit_mode) = &self.currently_editing {
+            match edit_mode{
+                CurrentlyEditing::Key => self.currently_editing = Some(CurrentlyEditing::Value), // Proceed to value
+                CurrentlyEditing::Value => self.currently_editing = Some(CurrentlyEditing::Key), // Proceed to key
+            }
+        }
+        else{
+                self.currently_editing = Some(CurrentlyEditing::Key); //Start with editing key
+            }
+    }
+
+    pub fn print_json(&self) -> serde_json::Result<()>{
+        let output = serde_json::to_string(&self.pairs);
+        println!("{:?}", output);
+        Ok(())
+    }
+}
+
